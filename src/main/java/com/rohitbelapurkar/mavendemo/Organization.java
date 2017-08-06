8545d819c878ff74e06c470f1e3d87f414094158
rohitrtb <rohitrtb@gmail.com>
2017-08-06 10:06:22 -0700

    Adding setter properties to the bean

--- src/main/java/com/rohitbelapurkar/mavendemo/Organization.java
+++ src/main/java/com/rohitbelapurkar/mavendemo/Organization.java
@@ -1,6 +1,33 @@
 package com.rohitbelapurkar.mavendemo;
 
 public class Organization {
+	
+	private String companyName;
+	private int yearOfIncorporation;
+	private int postalCode;
+	private int employeeCount;
+	
+	//Constructor for this class
+	public Organization(String companyName, int yearOfIncorporation) {
+		super();
+		this.companyName = companyName;
+		this.yearOfIncorporation = yearOfIncorporation;
+	}
+	
+	public void setEmployeeCount(int employeeCount) {
+		this.employeeCount = employeeCount;
+	}
+
+	public void setPostalCode(int postalCode) {
+		this.postalCode = postalCode;
+	}
+
+	@Override
+	public String toString() {
+		return "Organization [companyName=" + companyName + ", yearOfIncorporation=" + yearOfIncorporation
+				+ ", postalCode=" + postalCode + ", employeeCount=" + employeeCount + "]";
+	}
+
 	public void corporateSlogan() {
 		String slogan = "We build ultimate driving machines";
 		System.out.println(slogan);
---
+++ src/main/java/com/rohitbelapurkar/mavendemo/SetterApp.java
@@ -0,0 +1,25 @@
+package com.rohitbelapurkar.mavendemo;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class SetterApp {
+	
+	public static void main(String[] args) {
+		//getting the application context from the class path
+		ApplicationContext context = new ClassPathXmlApplicationContext("beans-cp.xml");
+		
+		//Getting the bean from the IOC container
+		Organization org = (Organization) context.getBean("myorg");
+		
+		//calling the corporateSlogan method to print out the slogan message
+		org.corporateSlogan();
+		
+		//Print organization details
+		System.out.println(org);
+		
+		//Close the application context
+		((ClassPathXmlApplicationContext)context).close();
+	}
+
+}
